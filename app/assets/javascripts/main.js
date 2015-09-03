f6ee80c64a5a74750d96a026afeacdffab9e0b09
Eric <Eric-luo@live.ca>
2015-09-02 23:13:19 -0400

    add show code button

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,8 +4,7 @@
 //     $('.defaultTable').sortable()
 // }) 
 $(document).ready(function(){
-	$(".hide-picture").click(function() {	
-		window.alert("Test")
+	$(".hide-picture").click(function() {
 		$('.toggle').slideToggle()
 	})
 
@@ -15,24 +14,26 @@ $(document).ready(function(){
       // transcript output:
       var fileInput = $('#file_upload')[0]; // or var fileInput = document.getElementById('fileInput');
       var fileDisplayArea = document.getElementById('fileDisplayArea');
-        $("#submit_file").click(function() {
-            
-            var file = fileInput.files[0];
-            var textType = /text.*/;
+            $("#submit_file").click(function() {
+                  var file = fileInput.files[0];
+                  var textType = /text.*/;
 
-            var reader = new FileReader();
-            reader.readAsText(file);  
-            reader.onload = function(e) {
-                  var x = reader.result.split("\r\n")
-                  // fileDisplayArea.innerText = x[6];
-                  // var x = "line1\nline2"
-                  $(".trans_output").prepend("<h4 class='text-center'>Here is the output: </h4>" + 
-                        "<br><li><strong>Company Name: </strong></li>" + x[6] + 
-                        "<li><strong>Ticker: </strong></li>" + x[6].split("-")[0] +
-                        "<li><strong>Event: </strong></li>" + x[7] +
-                        "<li><strong>Date: </strong></li>" + x[8]
+                  var reader = new FileReader();
+                  reader.readAsText(file);  
+                  reader.onload = function(e) {
+                        var x = reader.result.split("\r\n")
+                        $(".trans_output").prepend("<h4 class='text-center'>Here is the output: </h4>" + 
+                              "<br><li><strong>Company Name: </strong></li>" + x[6].split("-")[1] + 
+                              "<li><strong>Ticker: </strong></li>" + x[6].split("-")[0] +
+                              "<li><strong>Event: </strong></li>" + x[7] +
+                              "<li><strong>Date: </strong></li>" + x[8]
                         );
-            }  
-        });
+                  }  
+            });
+
+      $(".hide-code").click(function() {
+            $('.toggle').slideToggle()
+            $('.code_display').css('display','initial')
+      })
 })
 
